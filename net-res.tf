a7e90bf5e81bc7d77422176a47eff05f5dc6feb2
Sachin Ladde <41670385+sachinladde@users.noreply.github.com>
2019-12-26 11:59:17 +0000

    Add files via upload

---
+++ net-res.tf
@@ -0,0 +1,58 @@
+
+data "azurerm_resource_group" "res_group"{
+name  = "RG-TAdmin2-TD1"
+//location  = "westeurope"
+}
+
+data azurerm_virtual_network "Vnet" {
+    name = "TAdmin2-Network1"
+    //address_space = ["10.5.0.0/16"]
+    //location = "${azurerm_resource_group.res_group.location}"
+    resource_group_name = "${data.azurerm_resource_group.res_group.name}"
+}
+output "virtual_network_id" {
+  value = "${data.azurerm_virtual_network.Vnet.id}"
+}
+
+data azurerm_subnet "subnet" {
+    name = "TDEv-Development"
+    //address_prefix = "10.5.40.0/22"
+    virtual_network_name = "${data.azurerm_virtual_network.Vnet.name}"
+    resource_group_name = "${data.azurerm_resource_group.res_group.name}"
+}
+output "subnet_id" {
+  value = "${data.azurerm_subnet.subnet.id}"
+}
+
+resource "azurerm_network_interface" "NIC" {
+  name= "${var.prefix}-NIC"
+  location = "${data.azurerm_resource_group.res_group.location}"
+  resource_group_name = "${data.azurerm_resource_group.res_group.name}"
+
+  ip_configuration {
+      name = "${var.prefix}-nicConfig"
+      subnet_id= "${data.azurerm_subnet.subnet.id}"
+      private_ip_address_allocation="Dynamic"
+      
+  }
+}
+
+#/.. get public IP (need to check with Tressa)
+#data "azurerm_public_ip" "pip" {
+#name = "${azurerm_public_ip.datasourceip.name}"
+#resource_group_name = "${data.azurerm_resource_group.res_group.name}"
+#depends_on = ["azurerm_virtual_machine.test"]
+#}
+
+#output "ip_address" {
+#value = "${data.azurerm_public_ip.pip.ip_address}"
+#}
+
+data "azurerm_network_security_group" "netsecgrp" {
+  name                = "AADDS-ta2-dev.info-NSG"
+  resource_group_name = "${data.azurerm_resource_group.res_group.name}"
+}
+
+output "location" {
+  value = "${data.azurerm_network_security_group.netsecgrp.location}"
+}
