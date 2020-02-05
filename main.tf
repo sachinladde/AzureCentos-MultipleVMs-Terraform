cfa92a95d8732a35d4be1a73ede2f5fbc953e475
sachinladde <sachin.100185@gmail.com>
2020-02-05 13:15:37 +0000

    added secrets

--- main.tf
+++ main.tf
@@ -113,6 +113,7 @@ default = "latest"
 }
 variable "VM_ADMIN" {
 //Disallowed values: "administrator", "admin", "user", "user1", "test", "user2", "test1", "user3", "admin1", "1", "123", "a", "actuser", "adm", "admin2", "aspnet", "backup", "console", "david", "guest", "john", "owner", "root", "server", "sql", "support", "support_388945a0", "sys", "test2", "test3", "user4", "user5".
+default= "nancy"
 }
 
 
