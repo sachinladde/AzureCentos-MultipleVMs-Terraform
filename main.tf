ee221318242f7c9777f0b4c16ee791a611ced346
sachinladde <sachin.100185@gmail.com>
2020-02-27 09:59:51 +0000

    modified jenkins file

--- main.tf
+++ main.tf
@@ -20,16 +20,16 @@ terraform {
 
 provider "azurerm" {
 version = "~> 1.25"
-subscription_id="${var.subscription_id}"
-client_id="${var.client_id}"
-client_secret="${var.client_secret}"
-tenant_id="${var.tenant_id}"
+//subscription_id="${var.subscription_id}"
+//client_id="${var.client_id}"
+//client_secret="${var.client_secret}"
+//tenant_id="${var.tenant_id}"
   }
 
-variable "subscription_id"{}
-variable "client_id"{}
-variable "client_secret"{}
-variable "tenant_id"{}
+//variable "subscription_id"{}
+//variable "client_id"{}
+//variable "client_secret"{}
+//variable "tenant_id"{}
 
 
 variable "environment" {
